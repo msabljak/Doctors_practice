1b2ab0f1521871df65e310920592743d80167dbe
Martin Sabljak <sabljakmartin@outlook.com>
2020-08-27 15:32:26 +0200

    Created a resilient method that simulates charging a patient with corresponding tests

--- Doctors_practice.Tests/ChargePatientHandlerTest.cs
+++ Doctors_practice.Tests/ChargePatientHandlerTest.cs
@@ -10,6 +10,7 @@ using System.Threading;
 using Polly.Timeout;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Net.Http;
 
 namespace Doctors_practice.Tests
 {
@@ -22,17 +23,139 @@ namespace Doctors_practice.Tests
             chargingSystem.Setup(cs => cs.SimulateCharge(It.IsAny<double>())).Returns(true);
             var DB = new Mock<IDummyDB>();
             DB.Setup(db => db.PersistTransaction(It.IsAny<double>())).Returns(true);
+            var customer = new Customer(chargingSystem.Object, DB.Object);
+            customer.Balance = 500;
+            //customer.SetupGet(x => x.Balance).Returns(500);
+            //customer.Setup(x => x.Charge(It.IsAny<double>())).Returns(Task.FromResult(true));            
+            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer, 1);
+            ChargePatientCommand chargePatientCommand = new ChargePatientCommand(5);
+            CancellationToken cancellationToken = new CancellationToken();
+
+
+            Func<Task> action = () => chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
+            //Func<Task<bool>> functionWrapper = async () =>
+            //{
+            //    var res = await chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
+            //    return res;
+            //};
+
+            await Assert.ThrowsAsync<TimeoutRejectedException>(action);
+        }
+        [Fact]
+        public async void Handle_MockedTaskTooLongShouldFail()
+        {
             var customer = new Mock<ICustomer>();
             customer.SetupGet(x => x.Balance).Returns(500);
-           // customer.Setup(x => x.Charge(It.IsAny<double>())).Returns(Task.FromResult(true));            
-            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer.Object, 5);
-            chargePatientHandler.Delay = TimeSpan.FromSeconds(10);
+            customer.Setup(x => x.Charge(It.IsAny<double>())).Returns(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return true;
+            });
+            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer.Object, 1);
             ChargePatientCommand chargePatientCommand = new ChargePatientCommand(5);
             CancellationToken cancellationToken = new CancellationToken();
 
             Func<Task> action = () => chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
 
             await Assert.ThrowsAsync<TimeoutRejectedException>(action);
+        }
+
+        [Fact]
+        public async void Handle_SingleUncompletedChargeShouldWork()
+        {
+            var expected = true;
+            var customer = new Mock<ICustomer>();
+            customer.SetupGet(x => x.Balance).Returns(500);
+            customer.SetupSequence(x => x.Charge(It.IsAny<double>())).
+                Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return false;
+                })
+                .Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return true;
+                });
+            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer.Object, 50, 3);
+            ChargePatientCommand chargePatientCommand = new ChargePatientCommand(5);
+            CancellationToken cancellationToken = new CancellationToken();
+
+            var actual = await chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async void Handle_MultipleUncompletedChargesLessThanMaxRetriesShouldWork()
+        {
+            var expected = true;
+            var customer = new Mock<ICustomer>();
+            customer.SetupGet(x => x.Balance).Returns(500);
+            customer.SetupSequence(x => x.Charge(It.IsAny<double>()))
+                .Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return false;
+                })
+                .Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return false;
+                })
+                .Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return true;
+                });
+            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer.Object, 50, 3);
+            ChargePatientCommand chargePatientCommand = new ChargePatientCommand(5);
+            CancellationToken cancellationToken = new CancellationToken();
+
+            var actual = await chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async void Handle_MultipleUncompletedChargersGreaterThanMaxRetriesShouldFail()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.SetupGet(x => x.Balance).Returns(500);
+            customer.Setup(x => x.Charge(It.IsAny<double>()))
+                .Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return false;
+                });
+            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer.Object, 50, 3);
+            ChargePatientCommand chargePatientCommand = new ChargePatientCommand(5);
+            CancellationToken cancellationToken = new CancellationToken();
+
+            Func<Task> action = () => chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
+
+            await Assert.ThrowsAsync<HttpRequestException>(action);
+        }
+
+        [Fact]
+        public async void Handle_ValidArgumentsShouldWork()
+        {
+            var expected = true;
+            var customer = new Mock<ICustomer>();
+            customer.SetupGet(x => x.Balance).Returns(500);
+            customer.SetupSequence(x => x.Charge(It.IsAny<double>()))
+                .Returns(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    return true;
+                });
+            ChargePatientHandler chargePatientHandler = new ChargePatientHandler(customer.Object, 50, 3);
+            ChargePatientCommand chargePatientCommand = new ChargePatientCommand(5);
+            CancellationToken cancellationToken = new CancellationToken();
+
+            var actual = await chargePatientHandler.Handle(chargePatientCommand, cancellationToken);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Doctors_practice/CommandHandlers/ChargePatientHandler.cs
+++ Doctors_practice/CommandHandlers/ChargePatientHandler.cs
@@ -17,32 +17,28 @@ namespace Doctors_practice.CommandHandlers
 {
     public class ChargePatientHandler : IRequestHandler<ChargePatientCommand, bool>
     {
-        private readonly AsyncTimeoutPolicy _timeoutPolicy;
         private readonly AsyncPolicyWrap _policyWrap;
-        private ICustomer _customer;
-        public int MaxRetries { get; set; }
+        private ICustomer _customer;        
         public int TimeoutInSeconds { get; set; }
-        public TimeSpan Delay { get; set; }
-        public ChargePatientHandler(ICustomer customer, int timeoutInSeconds)
+        public int MaxRetries { get; set; }
+        public ChargePatientHandler(ICustomer customer, int timeoutInSeconds, int maxRetries = 0)
         {
             TimeoutInSeconds = timeoutInSeconds;
-            _timeoutPolicy = Policy.TimeoutAsync(TimeoutInSeconds, TimeoutStrategy.Pessimistic);
+            MaxRetries = maxRetries;
+            var _timeoutPolicy = Policy.TimeoutAsync(TimeoutInSeconds, TimeoutStrategy.Pessimistic);
             var _retryPolicy = Policy.Handle<HttpRequestException>().RetryAsync(MaxRetries);
-            //_policyWrap = Policy.WrapAsync(_timeoutPolicy, _retryPolicy);
+            _policyWrap = Policy.WrapAsync(_timeoutPolicy, _retryPolicy);
             _customer = customer;
         }
         public async Task<bool> Handle(ChargePatientCommand request, CancellationToken cancellationToken)
         {
-            return await _timeoutPolicy.ExecuteAsync(async () => 
+            return await _policyWrap.ExecuteAsync(async () => 
             {
-                ////Does not work, function is never actually ever called so the delay within does not activate
-                //await _customer.Charge(300);
-                //Does work and will call timeout exception if configured in a manner it should
-                await Task.Delay(Delay);
-                //if (result == false)
-                //{
-                //    throw new HttpRequestException("This is a fake request Exception");
-                //}
+                var result = await _customer.Charge(300);
+                if (result == false)
+                {
+                    throw new HttpRequestException("This is a fake request Exception");
+                }
                 return true;
             });
         }
