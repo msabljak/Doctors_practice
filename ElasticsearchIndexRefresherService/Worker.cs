9a1f7c803c4cdef492165df8c698a627eed0f59d
Martin Sabljak <sabljakmartin@outlook.com>
2020-09-25 15:38:31 +0200

    Added background worker for refreshing elasticsearch index

---
+++ ElasticsearchIndexRefresherService/Worker.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ElasticsearchIndexRefresherService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            HttpClient client = new HttpClient();
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                HttpResponseMessage response = await client.GetAsync(_configuration.GetConnectionString("doctorsPracticeApi"));
+                await Task.Delay(3600000, stoppingToken);
+            }
+        }
+    }
+}
